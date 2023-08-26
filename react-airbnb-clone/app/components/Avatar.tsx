10a93bcbee484fd125a3b3c61ce95a49203013ad
Kordian <kordianlukask@gmail.com>
2023-08-26 12:45:53 +0200

    add the navbar component

---
+++ react-airbnb-clone/app/components/Avatar.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Image from 'next/image';
+
+const Avatar = () => {
+  return (
+    <Image
+      className='
+                rounnded-full
+            '
+      height='30'
+      width='30'
+      alt='Avatar'
+      src='/images/placeholder.jpg'
+    />
+  );
+};
+
+export default Avatar;
