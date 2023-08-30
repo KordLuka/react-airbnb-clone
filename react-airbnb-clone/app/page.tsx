287b92427415382af6e2aed89cc16bb22c400783
Kordian <kordianlukask@gmail.com>
2023-08-30 11:44:48 +0200

    fix build

--- react-airbnb-clone/app/page.tsx
+++ react-airbnb-clone/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import getListings, { IListingParams } from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
